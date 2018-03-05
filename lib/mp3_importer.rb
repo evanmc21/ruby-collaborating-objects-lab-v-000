f233f218fd0f3d867f0917ed76f0ba610542df13
Evan Candler <emcandler21@gmail.com>
2018-03-05 17:35:48 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,11 +2,15 @@ class MP3Importer
   attr_accessor :path
 
   @path = []
+  
   def initialize(directory)
     @path = directory
   end
 
   def files
-    @files
+    @files = Dir.entries(@path)
+    @files.delete_if{|file| file == "." || file == ".."}
   end
+
+  
 end
