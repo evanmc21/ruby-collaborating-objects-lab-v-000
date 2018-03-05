d84d21fab5b536a1120458b318818380acc1aba4
Evan Candler <emcandler21@gmail.com>
2018-03-05 18:37:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,9 @@ class Song
 
   def self.new_by_filename(filename)
     song, artist = filename.split(" - ")
+    new_tune = self.new(song)
+    new_tune.artist_name = artist
+    new_tune
+  end
 
 end
