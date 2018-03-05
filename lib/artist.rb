27b670598e32b06020abd31db79b8d113179bb81
Evan Candler <emcandler21@gmail.com>
2018-03-05 18:56:20 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,6 +21,12 @@ class Artist
     @@all
   end
 
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
+
+end
+
   def self.create_by_name(name)
       self.new(name).tap {|artist| artist.save} #use tap to simplify the method chain.
     end
@@ -29,10 +35,7 @@ class Artist
     self.all.find_by_name {|artist| artist.name == name}
   end
 
-  def self.find_or_create_by_name(name)
-    self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
 
-end
 
 def print_songs
   songs.each {|song| puts song.name}
